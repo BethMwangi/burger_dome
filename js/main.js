3b7c6ca336d894106dca3c4ab3f714e58fe873b1
BethMwangi <wanjikumwangib@gmail.com>
2016-03-25 10:21:28 +0300

    Adding google map

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//if (document.getElementById('map-canvas')) {
+
+//    var myLatlng = new google.maps.LatLng(52.525595, 13.393085);
+
+//   var mapOptions = {
+///       zoom: 15,
+//   center: myLatlng,
+// mapTypeId: google.maps.MapTypeId.ROADMAP
+//   };
+//
+//   var map = new google.maps.Map(documentGetElementById("map-// canvas"), mapOptions);
+// }
+function initMap() {
+    var mapDiv = document.getElementById('map-canvas');
+    var map = new google.maps.Map(mapDiv, {
+        zoom: 8,
+        center: new google.maps.LatLng(-34.397, 150.644)
+    });
+
+    // We add a DOM event here to show an alert if the DIV containing the
+    // map is clicked.
+    google.maps.event.addDomListener(mapDiv, 'click', function () {
+        window.alert('Map was clicked!');
+    });
+}
