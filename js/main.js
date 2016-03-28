3e40c531810b6bee5ce50591f2cd7d47687a0c58
BethMwangi <wanjikumwangib@gmail.com>
2016-03-29 01:13:39 +0300

    Setting map coordinates with API

--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
-//if (document.getElementById('map-canvas')) {
-
-//    var myLatlng = new google.maps.LatLng(52.525595, 13.393085);
-
-//   var mapOptions = {
-///       zoom: 15,
-//   center: myLatlng,
-// mapTypeId: google.maps.MapTypeId.ROADMAP
-//   };
-//
-//   var map = new google.maps.Map(documentGetElementById("map-// canvas"), mapOptions);
-// }
-var map;
-
-function initMap() {
-    var mapDiv = document.getElementById('map-canvas');
-    var map = new google.maps.Map(mapDiv, {
-        zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644)
-    });
-
-    // We add a DOM event here to show an alert if the DIV containing the
-    // map is clicked.
-    google.maps.event.addDomListener(mapDiv, 'click', function () {
-        window.alert('Map was clicked!');
-    });
-}
+< script type = 'text/javascript' >
+    function init_map() {
+        var myOptions = {
+            zoom: 12,
+            center: new google.maps.LatLng(52.2296756, 21.012228700000037),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+        marker = new google.maps.Marker({
+            map: map,
+            position: new google.maps.LatLng(52.2296756, 21.012228700000037)
+        });
+        infowindow = new google.maps.InfoWindow({
+            content: '<strong>Warsaw, Poland</strong><br><br> warsaw<br>'
+        });
+        google.maps.event.addListener(marker, 'click', function () {
+            infowindow.open(map, marker);
+        });
+        infowindow.open(map, marker);
+    }
+google.maps.event.addDomListener(window, 'load', init_map); < /script>
